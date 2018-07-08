fb610db58fb1eec8c0ad2a004aa250a156706ee9
meHoLi <346857553@qq.com>
2018-07-08 19:25:06 +0800

    API

---
+++ ClassScheduleAPI/Controllers/ChildrenController.cs
@@ -0,0 +1,79 @@
+﻿using ClassScheduleAPI.Common;
+using ClassScheduleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassScheduleAPI.Controllers
+{
+    public class ChildrenController : Controller
+    {
+        // GET: Children
+        public ActionResult Index(string openID)
+        {
+            using (ClassScheduleDBEntities db = new ClassScheduleDBEntities())
+            {
+                var list = db.Children.Where(p => p.OpenID == openID).ToList();
+                return Json(list, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Add(Children model)
+        {
+            using (ClassScheduleDBEntities db = new ClassScheduleDBEntities())
+            {
+                ResponseMessage msg = new ResponseMessage();
+                try
+                {
+                    var entity = db.Children.Add(model);
+                    db.SaveChanges();
+                    msg.Status = true;
+                }
+                catch (Exception e)
+                {
+                    msg.Status = false;
+                }
+                return Json(msg);
+            }
+        }
+        public ActionResult Update(Children model)
+        {
+            using (ClassScheduleDBEntities db = new ClassScheduleDBEntities())
+            {
+                ResponseMessage msg = new ResponseMessage();
+                try
+                {
+                    //https://blog.csdn.net/u011872945/article/details/72966946
+                    db.Children.Attach(model);
+                    db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    msg.Status = true;
+                }
+                catch (Exception e)
+                {
+                    msg.Status = false;
+                }
+                return Json(msg);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (ClassScheduleDBEntities db = new ClassScheduleDBEntities())
+            {
+                ResponseMessage msg = new ResponseMessage();
+                try
+                {
+                    db.Database.ExecuteSqlCommand("delete Children where id= " + id);
+                    msg.Status = true;
+                }
+                catch (Exception e)
+                {
+                    msg.Status = false;
+                }
+                return Json(msg);
+            }
+        }
+    }
+}
